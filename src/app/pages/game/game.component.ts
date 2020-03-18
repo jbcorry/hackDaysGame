6dd1ba5787e3cb95d2ca0ae399a65ee596709fb7
Jacob Corry <jcorry@slcm-jcorry.lan>
2020-03-18 13:57:04 -0600

    cleanup

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -19,7 +19,7 @@ export class GameComponent implements OnInit {
   add: any;
   constructor() {
     this.config = {
-      title: 'Sample',
+      title: 'Deploy Game',
       type: Phaser.AUTO,
       scale: {
         width: window.innerWidth,
@@ -34,7 +34,7 @@ export class GameComponent implements OnInit {
       },
       parent: 'game',
       backgroundColor: '#000000',
-      scene: GameScene
+      scene: [GameScene]
     };
   }
   ngOnInit() {
@@ -193,117 +193,3 @@ export class GameScene extends Phaser.Scene {
     this.physics.add.collider(star, this.sand, this.onFall(star), null, this);
   }
 }
-
-
-
-
-
-//Starfall example
-
-// export class GameComponent implements OnInit {
-//   phaserGame: Phaser.Game;
-//   config: Phaser.Types.Core.GameConfig;
-//   constructor() {
-//     this.config = {
-//       title: 'Starfall',
-//       type: Phaser.AUTO,
-//       scale: {
-//         width: window.innerWidth,
-//         height: window.innerHeight,
-//       },
-//       physics: {
-//         default: 'arcade',
-//         arcade: {
-//           debug: true,
-//         },
-//       },
-//       parent: 'game',
-//       backgroundColor: '#18216D',
-//       scene: [StarFallScene]
-//     };
-//   }
-//   ngOnInit() {
-//     this.phaserGame = new Phaser.Game(this.config);
-//   }
-// }
-
-// class StarFallScene extends Phaser.Scene {
-//   delta: number;
-//   lastStarTime: number;
-//   starsCaught: number;
-//   starsFallen: number;
-//   sand: Phaser.Physics.Arcade.StaticGroup;
-//   info: Phaser.GameObjects.Text;
-//   constructor() {
-//       super({
-//         key: "StarFallScene"
-//       });
-//     }
-//   init(params): void {
-//     this.delta = 1000;
-//     this.lastStarTime = 0;
-//     this.starsCaught = 0;
-//     this.starsFallen = 0;
-//   }
-//   preload(): void {
-//     this.load.setBaseURL(
-//       "https://raw.githubusercontent.com/mariyadavydova/" +
-//       "starfall-phaser3-typescript/master/");
-//     this.load.image("star", "assets/star.png");
-//     this.load.image("sand", "assets/sand.jpg");
-//     }
-//     create(): void {
-//       this.sand = this.physics.add.staticGroup({
-//         key: 'sand',
-//         frameQuantity: 20
-//       });
-//       Phaser.Actions.PlaceOnLine(this.sand.getChildren(),
-//         new Phaser.Geom.Line(20, 580, 820, 580));
-//       this.sand.refresh();
-//       this.info = this.add.text(10, 10, '', { font: '24px Arial Bold', fill: '#FBFBAC' });
-//     }
-//   update(time: number): void {
-//     const diff: number = time - this.lastStarTime;
-//     if (diff > this.delta) {
-//       this.lastStarTime = time;
-//       if (this.delta > 500) {
-//         this.delta -= 20;
-//       }
-//       this.emitStar();
-//     }
-//     this.info.text =
-//       this.starsCaught + " caught - " +
-//       this.starsFallen + " fallen (max 3)";
-//   }
-//   private onClick(star: Phaser.Physics.Arcade.Image): () => void {
-//     return function () {
-//       star.setTint(0x00ff00);
-//       star.setVelocity(0, 0);
-//       this.starsCaught += 1;
-//       this.time.delayedCall(100, function (star) {
-//         star.destroy();
-//       }, [star], this);
-//     }
-//   }
-//   private onFall(star: Phaser.Physics.Arcade.Image): () => void {
-//     return function () {
-//       star.setTint(0xff0000);
-//       this.starsFallen += 1;
-//       this.time.delayedCall(100, function (star) {
-//         star.destroy();
-//       }, [star], this);
-//     }
-//   }
-//   private emitStar(): void {
-//     var star: Phaser.Physics.Arcade.Image;
-//     var x = Phaser.Math.Between(25, 775);
-//     var y = 26;
-//     star = this.physics.add.image(x, y, "star");
-//     star.setDisplaySize(50, 50);
-//     star.setVelocity(0, 200);
-//     star.setInteractive();
-//     star.on('pointerdown', this.onClick(star), this);
-//     this.physics.add.collider(star, this.sand, 
-//       this.onFall(star), null, this);
-//   }
-// }
