b71df72bc7c147ac07dfdb99f7c8453abe9c3aac
Stephen Corry <scorry@slcm-scorry.lan>
2020-03-18 09:11:09 -0600

    firebase and routing build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GameComponent } from './pages/game/game.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'game', component: GameComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
